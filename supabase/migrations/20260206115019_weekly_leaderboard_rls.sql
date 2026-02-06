731ea2f47e3d60c88a6903d76f32d85fbb44e175
Jayesh Mohanani <jayesh30801@gmail.com>
2026-02-06 16:28:37 +0400

    Removed unnecesary files.

--- supabase/migrations/20260206115019_weekly_leaderboard_rls.sql
+++
@@ -1,9 +0,0 @@
--- Enable RLS on weekly_leaderboard
-ALTER TABLE public.weekly_leaderboard ENABLE ROW LEVEL SECURITY;
-
--- Allow authenticated users to read leaderboard (public leaderboard)
-CREATE POLICY "Allow authenticated read access"
-ON public.weekly_leaderboard
-FOR SELECT
-TO authenticated
-USING (true);
